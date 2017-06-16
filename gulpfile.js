6b2ea82608a419b5f97011e7a2a232fa5dc4ff5b
Irina <irinazinevitch@gmail.com>
2017-06-16 10:40:49 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 var gulp            = require('gulp'),
     less            = require('gulp-less'),
-    browserSync     = require('browser-sync'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglifyjs'),
-    del             = require('del');
+    autoprefixer    = require('gulp-autoprefixer');
 
 
 gulp.task('less', function () {
@@ -14,49 +10,10 @@ gulp.task('less', function () {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('browser-sync', function () {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        },
-        notify: false
-    });
+
+gulp.task('watch', ['less'], function () {
+    gulp.watch('app/less/**/*.less', ['less']);
 });
-
-gulp.task('scripts', function () {
-    return gulp.src(['app/libs/jquery/dist/jquery.min.js'])
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
-});
-
-gulp.task('watch', ['browser-sync', 'less', 'scripts'], function () {
-    gulp.watch('app/less/*.less', ['less']);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
-gulp.task('clean', function () {
-    del.sync('dist');
-});
-
-gulp.task('build',['less', 'scripts'], function () {
-
-    var buildCss = gulp.src('app/css/main.css')
-        .pipe(gulp.dest('dist/css'));
-
-    var buildFonts = gulp.src('app/fonts/**/*.css')
-        .pipe(gulp.dest('dist/fonts'));
-
-    var buildJS = gulp.src('app/js/**/*')
-        .pipe(gulp.dest('dist/js'));
-
-    var buildHtml = gulp.src('app/*.html')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('default', ['watch']);
